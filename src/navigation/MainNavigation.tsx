5023ba77c2ebb13a9677c24b5bd71707eab8056e
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-01-09 22:15:56 +0530

    navigation changes

---
+++ src/navigation/MainNavigation.tsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AppStackParamList } from '../utils/types';
+import Home from '../screens/Home';
+
+const AppStack = createNativeStackNavigator<AppStackParamList>();
+
+function AppStackNavigator() {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name='Home' component={Home} />
+    </AppStack.Navigator>
+  );
+}
+
+export default AppStackNavigator;
