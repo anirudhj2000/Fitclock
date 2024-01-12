136bc1aef1245f314c2ed416989c736356aecfc1
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-01-12 17:19:01 +0530

    minor fixes

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppStackNavigator from './src/navigation/MainNavigation';
+import { colors } from './src/utils/colors';
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.primary,
     alignItems: 'center',
     justifyContent: 'center',
   },
