eb35c8099eb949fe1ddb6d928fbe50517a654fce
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-01-29 22:16:55 +0530

    minor fixes

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,6 +1,8 @@
+import React, { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AppStackParamList } from '../utils/types';
 import Home from '../screens/Home';
+import Circuits from '../screens/Circuits';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AppStack = createNativeStackNavigator<AppStackParamList>();
@@ -9,6 +11,7 @@ function AppStackNavigator() {
   return (
     <AppStack.Navigator screenOptions={{ headerShown: false }}>
       <AppStack.Screen name='Home' component={Home} />
+      <AppStack.Screen name='Circuits' component={Circuits} />
     </AppStack.Navigator>
   );
 }
