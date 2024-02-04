43ba5792da386937efa0ca1a57cb34ea7e49b3fc
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-02-04 16:44:04 +0530

    const changes

--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -3,3 +3,76 @@ export const images = [
   'https://drive.usercontent.google.com/download?id=1zrKdJDMJKUzvHGer46v2TA1rOEq2dmn6',
   'https://drive.usercontent.google.com/download?id=1rPzX1wCP-rd3wFIIrXliBYjZS4fT_b7I',
 ];
+
+export const dummyCircuit = [
+  {
+    title: 'Squats',
+    exercise: 'SQ',
+    type: 'duration',
+    value: 30,
+    rest: 15,
+  },
+  {
+    title: 'Push-ups',
+    exercise: 'PU',
+    type: 'reps',
+    value: 15,
+    rest: 10,
+  },
+  {
+    title: 'Plank',
+    exercise: 'PL',
+    type: 'duration',
+    value: 45,
+    rest: 20,
+  },
+  {
+    title: 'Lunges',
+    exercise: 'LN',
+    type: 'reps',
+    value: 20,
+    rest: 12,
+  },
+  {
+    title: 'Jumping Jacks',
+    exercise: 'JJ',
+    type: 'duration',
+    value: 60,
+    rest: 15,
+  },
+  {
+    title: 'Burpees',
+    exercise: 'BR',
+    type: 'reps',
+    value: 10,
+    rest: 18,
+  },
+  {
+    title: 'Mountain Climbers',
+    exercise: 'MC',
+    type: 'duration',
+    value: 40,
+    rest: 15,
+  },
+  {
+    title: 'Bicycle Crunches',
+    exercise: 'BC',
+    type: 'reps',
+    value: 25,
+    rest: 12,
+  },
+  {
+    title: 'Dumbbell Rows',
+    exercise: 'DR',
+    type: 'duration',
+    value: 40,
+    rest: 20,
+  },
+  {
+    title: 'Leg Raises',
+    exercise: 'LR',
+    type: 'reps',
+    value: 18,
+    rest: 15,
+  },
+];
