3d4f1344b7c70402a913933b48938808b331c560
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-02-05 13:55:05 +0530

    config changes

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -6,13 +6,37 @@ import AppStackNavigator from './src/navigation/MainNavigation';
 import { colors } from './src/utils/colors';
 import LoginStackNavigator from './src/navigation/AuthNavigation';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import useUserStore from './src/utils/store';
+import Toast from 'react-native-toast-message';
+import React from 'react';
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  let userData = useUserStore((state) => state.user);
+  let updateUser = useUserStore((state) => state.updateUser);
+  const [user, setUser] = React.useState<string | null>(null);
+
+  // const [user, setUser] = React.useState<any>(
+  //   useUserStore(state => state.user),
+  // );
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const fetchInfo = async () => {
+    let data = await AsyncStorage.getItem('user');
+    setUser(data);
+    updateUser(data);
+    console.log('user data', data);
+  };
   return (
-    <NavigationContainer>
-      {isLoggedIn ? <AppStackNavigator /> : <LoginStackNavigator />}
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Toast />
+      <NavigationContainer>
+        {userData ? <AppStackNavigator /> : <LoginStackNavigator />}
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
