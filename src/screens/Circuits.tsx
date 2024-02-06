c55257ed5edd92aa45617ebe4cc0e42d7905e029
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-02-06 15:37:48 +0530

    circuits screen ui

--- src/screens/Circuits.tsx
+++ src/screens/Circuits.tsx
@@ -1,8 +1,53 @@
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  Platform,
+  Dimensions,
+  FlatList,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { colors } from '../utils/colors';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import AppTitle from '../components/AppTitle';
+import { AppStackScreenProps } from '../utils/types';
+import useUserStore from '../utils/store';
+import firestore from '@react-native-firebase/firestore';
+import CircuitCard from '../components/CircuitCard';
 
-const Circuits = () => {
+const { height, width } = Dimensions.get('window');
+
+const Circuits = ({ navigation }: AppStackScreenProps) => {
+  const user = useUserStore((state) => state.user);
+  const [circuitsList, setCircuitsList] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    getCricuitList();
+  }, []);
+
+  const getCricuitList = () => {
+    console.log('user', user.email);
+    if (user.email)
+      firestore()
+        .collection('Circuits')
+        .where('user', '==', user?.email)
+        .orderBy('exercisesLength')
+        .limit(5)
+        .get()
+        .then((query) => {
+          let arr: any = [];
+
+          console.log('result', query.docs);
+          query.docs.map((item) => {
+            arr.push(item.data());
+          });
+          console.log('arr', arr);
+          setCircuitsList(arr);
+        });
+  };
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar
@@ -12,6 +57,82 @@ const Circuits = () => {
         showHideTransition={'slide'}
         hidden={false}
       />
+      <View
+        style={[
+          styles.basic,
+          {
+            marginTop: Platform.OS == 'android' ? '5%' : 0,
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginHorizontal: 0,
+          },
+        ]}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            // AsyncStorage.clear();
+            // auth().signOut();
+            // updateUser(null);
+            navigation.goBack();
+          }}
+        >
+          <Ionicons
+            name='chevron-back'
+            size={height * 0.04}
+            color='white'
+            style={{ marginRight: 4 }}
+          />
+        </TouchableOpacity>
+        <AppTitle text1='Cir' text2='cuits' fontSize={height * 0.035} />
+      </View>
+      <View style={{ display: 'flex', flexDirection: 'column' }}>
+        <FlatList
+          style={{ height: height * 0.8, overflow: 'hidden' }}
+          data={circuitsList}
+          keyExtractor={(item, index) => index.toString()}
+          ListEmptyComponent={() => {
+            return (
+              <View
+                style={{
+                  width: '100%',
+                  height: height * 0.6,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginTop: '10%',
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('CreateCircuits');
+                  }}
+                >
+                  <MaterialIcons
+                    name='add-circle'
+                    size={height * 0.05}
+                    color={colors.primary + 'aa'}
+                  />
+                </TouchableOpacity>
+                <Text
+                  style={{
+                    fontFamily: 'Inter_400Regular',
+                    color: colors.primary + 'aa',
+                    fontSize: 18,
+                    textAlign: 'center',
+                    marginTop: '1.5%',
+                  }}
+                >
+                  Add new circuits!!
+                </Text>
+              </View>
+            );
+          }}
+          renderItem={({ item, index }) => {
+            return <CircuitCard title={item.title} duration={item.duration} onClick={() => {}} />;
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
